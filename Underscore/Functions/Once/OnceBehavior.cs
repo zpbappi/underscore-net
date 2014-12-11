83fe0d089a170f96500fb80df359d09b348877d8
zpbappi <zpbappi@gmail.com>
2014-12-11 22:46:33 +0600

    added thread safety to _.Once()

--- Underscore/Functions/Once/OnceBehavior.cs
+++ Underscore/Functions/Once/OnceBehavior.cs
@@ -6,12 +6,37 @@
 
     internal class OnceBehavior : IExecutionBehavior
     {
+        private readonly object mutext;
+
+        private bool canExecute;
+
         public OnceBehavior()
         {
-            this.CanExecute = true;
+            this.canExecute = true;
+            this.mutext = new object();
         }
 
-        public bool CanExecute { get; private set; }
+        public bool CanExecute
+        {
+            get
+            {
+                if (!this.canExecute)
+                {
+                    return false;
+                }
+
+                lock (this.mutext)
+                {
+                    if (!this.canExecute)
+                    {
+                        return false;
+                    }
+
+                    this.canExecute = false;
+                    return true;
+                }
+            }
+        }
 
         public void NotifyWrapperCalling(Guid callerId, params object[] args)
         {
@@ -23,7 +48,6 @@
 
         public void NotifyExecuting(Guid callerId, params object[] args)
         {
-            this.CanExecute = false;
         }
 
         public void NotifyExecuted(Guid callerId, params object[] args)
