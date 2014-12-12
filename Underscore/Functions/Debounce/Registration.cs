502c91eb12e1f0d1126ee8b158191107d782d2c4
zpbappi <zpbappi@gmail.com>
2014-12-12 16:52:50 +0600

    added registration for _.Debounce() method

---
+++ Underscore/Functions/Debounce/Registration.cs
@@ -0,0 +1,20 @@
+﻿namespace Underscore.Functions.Once
+{
+    using System;
+
+    using global::Underscore.Core;
+    using global::Underscore.Core.Generic;
+    using global::Underscore.Functions.Debounce;
+
+    /// <summary>
+    /// Registration for Debounce() methods
+    /// </summary>
+    public static partial class Underscore
+    {
+        public static Action Debounce(Action action, double wait, bool immediate = false)
+        {
+            var behavior = immediate ? (IExecutionBehavior)null : new TrailingDebounceBehavior(wait);
+            return new ExecutionContext(action, behavior).Wrapper;
+        }
+    }
+}
