6f193db4c9cb0b2a7c83485008bd0dbd52fddb49
zpbappi <zpbappi@gmail.com>
2014-12-30 22:06:44 +0600

    added throttle behavior and registraion

---
+++ Underscore/Functions/Throttle/Registration.cs
@@ -0,0 +1,51 @@
+﻿namespace UnderscoreNet
+{
+    using System;
+
+    using UnderscoreNet.Core;
+    using UnderscoreNet.Core.Generic;
+    using UnderscoreNet.Functions.Throttle;
+
+    /// <summary>
+    /// Registration for Throttle() methods
+    /// </summary>
+    public static partial class Registration
+    {
+        public static Action Throttle(Action action, double wait, bool leading = true, bool trailing = false)
+        {
+            return new ExecutionContext(action, new ThrottleBehavior(wait, leading, trailing)).Wrapper;
+        }
+
+        public static Action<T> Throttle<T>(Action<T> action, double wait, bool leading = true, bool trailing = true)
+        {
+            return new ExecutionContext<T>(action, new ThrottleBehavior(wait, leading, trailing)).Wrapper;
+        }
+
+        public static Action<T1, T2> Throttle<T1, T2>(
+            Action<T1, T2> action,
+            double wait,
+            bool leading = true,
+            bool trailing = true)
+        {
+            return new ExecutionContext<T1, T2>(action, new ThrottleBehavior(wait, leading, trailing)).Wrapper;
+        }
+
+        public static Action<T1, T2, T3> Throttle<T1, T2, T3>(
+            Action<T1, T2, T3> action,
+            double wait,
+            bool leading = true,
+            bool trailing = true)
+        {
+            return new ExecutionContext<T1, T2, T3>(action, new ThrottleBehavior(wait, leading, trailing)).Wrapper;
+        }
+
+        public static Action<T1, T2, T3, T4> Throttle<T1, T2, T3, T4>(
+            Action<T1, T2, T3, T4> action,
+            double wait,
+            bool leading = true,
+            bool trailing = true)
+        {
+            return new ExecutionContext<T1, T2, T3, T4>(action, new ThrottleBehavior(wait, leading, trailing)).Wrapper;
+        }
+    }
+}
---
+++ Underscore/Functions/Throttle/ThrottleBehavior.cs
@@ -0,0 +1,147 @@
+﻿namespace UnderscoreNet.Functions.Throttle
+{
+    using System;
+    using System.Threading;
+    using System.Timers;
+
+    using Microsoft.Win32;
+
+    using UnderscoreNet.Core;
+
+    using Timer = System.Timers.Timer;
+
+    internal class ThrottleBehavior : IExecutionBehavior
+    {
+        private readonly object leadingMutext;
+        private readonly Timer throttlingExecutionTimer;
+        private readonly Mutex trailingMutext;
+
+        private readonly bool leading;
+        private readonly bool trailing;
+        private IExecutionWrapper wrapper;
+        
+        private Guid? trailingCallerId;
+        private object[] trailingParameters;
+        private System.Threading.ExecutionContext trailingExecContext;
+
+        private bool canExecuteLeadingCall;
+
+        public ThrottleBehavior(double wait, bool leading, bool trailing)
+        {
+            this.leading = leading;
+            this.trailing = trailing;
+            this.leadingMutext = new object();
+            this.trailingExecContext = null;
+
+            this.canExecuteLeadingCall = true;
+
+            this.trailingMutext = new Mutex();
+            this.throttlingExecutionTimer = new Timer { AutoReset = false, Interval = wait, Enabled = false };
+            this.throttlingExecutionTimer.Elapsed += this.ThrottlingExecutionTimerElapsed;
+
+        }
+
+        public bool CanExecute
+        {
+            get
+            {
+                if (!this.leading)
+                {
+                    return false;
+                }
+
+                if (!this.canExecuteLeadingCall)
+                {
+                    return false;
+                }
+
+                lock (this.leadingMutext)
+                {
+                    if (!this.canExecuteLeadingCall)
+                    {
+                        return false;
+                    }
+
+                    this.canExecuteLeadingCall = false;
+                    return true;
+                }
+            }
+        }
+
+        public void NotifyWrapperCalling(Guid callerId, params object[] args)
+        {
+            if (!this.trailing)
+            {
+                return;
+            }
+
+            this.trailingMutext.WaitOne();
+            this.trailingCallerId = callerId;
+            this.trailingParameters = args;
+            if (this.trailingExecContext != null)
+            {
+                this.trailingExecContext.Dispose();
+            }
+
+            this.trailingExecContext = System.Threading.ExecutionContext.Capture();
+            this.trailingMutext.ReleaseMutex();
+            if (!this.throttlingExecutionTimer.Enabled)
+            {
+                this.throttlingExecutionTimer.Start();
+            }
+        }
+
+        public void NotifyWrapperCalled(Guid callerId, params object[] args)
+        {   
+        }
+
+        public void NotifyExecuting(Guid callerId, params object[] args)
+        {
+        }
+
+        public void NotifyExecuted(Guid callerId, params object[] args)
+        {
+            if (!this.leading && !this.trailing)
+            {
+                return;
+            }
+
+            this.throttlingExecutionTimer.Start();
+        }
+
+        public void SetWrapper(IExecutionWrapper executionWrapper)
+        {
+            this.wrapper = executionWrapper;
+        }
+
+        private void ThrottlingExecutionTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var timer = (Timer)sender;
+            timer.Stop();
+
+            if (this.trailing && this.trailingCallerId.HasValue)
+            {
+                this.trailingMutext.WaitOne();
+                using (this.trailingExecContext)
+                {
+                    System.Threading.ExecutionContext.Run(
+                        this.trailingExecContext,
+                        state => this.wrapper.Execute(this.trailingCallerId.Value, this.trailingParameters),
+                        null);
+                }
+
+                this.trailingExecContext = null;
+                this.trailingCallerId = null;
+                this.trailingParameters = null;
+                this.trailingMutext.ReleaseMutex();
+            }
+            else if (this.leading)
+            {
+                lock (this.leadingMutext)
+                {
+                    this.canExecuteLeadingCall = true;
+                }
+            }
+        }
+    }
+}
