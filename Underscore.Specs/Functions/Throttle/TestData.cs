bdc339977c594093ecfd78f7fa0bc334eed909b9
zpbappi <zpbappi@gmail.com>
2014-12-30 22:21:36 +0600

    added tests for non-generic version of throttle

---
+++ Underscore.Specs/Functions/Throttle/TestData.cs
@@ -0,0 +1,52 @@
+﻿namespace Underscore.Specs.Functions.Throttle
+{
+    using System.Collections.Generic;
+
+    public interface ITestData
+    {
+        void DoSomething();
+
+        void DoSomethingElse();
+
+        void Method1(int a);
+
+        void Method2(int a, string s);
+
+        void Method3(string a, double d, IEnumerable<string> names);
+
+        void Method4(int a, char b, string c, long d);
+    }
+
+    public class TestData : ITestData
+    {
+        public void DoSomething()
+        {
+            // do nothing
+        }
+
+        public void DoSomethingElse()
+        {
+            // do nothing
+        }
+
+        public void Method1(int a)
+        {
+            // do nothing
+        }
+
+        public void Method2(int a, string s)
+        {
+            // do nothing
+        }
+
+        public void Method3(string a, double d, IEnumerable<string> names)
+        {
+            // do nothing
+        }
+
+        public void Method4(int a, char b, string c, long d)
+        {
+            // do nothing
+        }
+    }
+}
